318f49a792eaf74f026e6e17c70122fa4378f347
kissmrton9 <kissmrton9@gmail.com>
2021-07-09 11:09:53 +0200

    feladat-02 by hand

---
+++ mongo-feladat-02/directors.js
@@ -0,0 +1,20 @@
+[
+  {
+    "_id": 1,
+    "name": "James Cameron",
+    "birthYear": 1958,
+    "movies": []
+  },
+  {
+    "_id": 2,
+    "name": "Clint Eastwood",
+    "birthYear": 1935,
+    "movies": []
+  },
+  {
+    "_id": 3,
+    "name": "Steven Spielberg",
+    "birthYear": 1930,
+    "movies": []
+  }
+]
